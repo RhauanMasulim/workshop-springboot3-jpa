b4b29ddeda86a54eaee0e46fc9e8e7452b9bf195
Rhauan Masulim <rmasulim@gmail.com>
2023-07-05 17:01:22 -0300

    User entity and resources

---
+++ src/main/java/com/rhauan/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.rhauan.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rhauan.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1l,"Maria","maria@gmail.com","99999999","123");
+		return ResponseEntity.ok().body(u);
+	}
+}
